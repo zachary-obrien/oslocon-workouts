f141a0fc22288ae979ad96c456b17d296253133b
stitchclipper@gmail.com <stitchclipper@gmail.com>
2026-04-10 14:54:11 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 class Form1(Form1Template):
   def __init__(self, **properties):
